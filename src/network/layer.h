028fe7e57780e279ac52a890111c6fa304b92b6f
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-14 11:14:06 +0530

    private members added

--- src/network/layer.h
+++ src/network/layer.h
@@ -10,10 +10,15 @@ class Layer{
         std::vector<double> forward(std::vector<double>& inputs);
         void compute_gradient(const std::vector<double>& delta_next_layer);
         void update_weights(double learning_rate);
-    private:
+    
         std::vector<Neuron>& get_neurons;
         const std::vector<double>& get_activations;
         const std::vector<double>& get_deltas;
+
+    private:
+        std::vector<Neuron> neurons;
+        std::vector<double> activations;
+        std::vector<double> deltas;
 };
 
 #endif
