6fc47c6806b28678c8e260ad36d3dedff0c9069c
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-14 11:37:12 +0530

    gradients computed

--- src/network/layer.cpp
+++ src/network/layer.cpp
@@ -1,1 +1,27 @@
- 
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include "layer.h"
+
+Layer::Layer(int no_neurons,int input_size){
+    for(size_t i = 0; i < no_neurons; i ++){
+        neurons.push_back(Neuron(input_size));
+    }
+}
+
+std::vector<double> Layer::forward(std::vector<double>& inputs){
+    activations.clear();
+    for (auto &neuron: neurons){
+        activations.push_back(neuron.activate(inputs));
+    }
+    return activations;
+}   
+
+void Layer::compute_gradient(const std::vector<double>& delta_next_layer){
+    deltas.resize(neurons.size());
+    for (size_t i = 0;i < neurons.size(); i ++ ){
+        double activation = activations[i];
+        deltas[i] = sigmoid_derivative(activation) * delta_next_layer[i];
+    }
+}
