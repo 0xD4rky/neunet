4d66b051cad03abdf27ad75d5a214b164fcc6d87
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-29 23:30:25 +0530

    main impl

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
- 
+#include "network/neuron.h"
+#include "misc/functions.h"
+#include "dataset/data.h"
+
+#include <ctime>
+#include <numeric>
+#include <fstream>
+
+
+#include <iostream>
+
+#include <vector>
+#include <limits>
+#include <thread>
+
+
