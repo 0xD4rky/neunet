316452dbd8412d81cdd0d62443cba52acc29bc94
Ishaan Kumar <ishaan.kumar281@gmail.com>
2024-08-29 23:33:07 +0530

    functions

--- src/misc/functions.h
+++ src/misc/functions.h
@@ -18,7 +18,7 @@ double sigmoid_derivative(double x);
 double relu(double x);
 double relu_derivative(double x);
 
-//Random float getter function
+//Random float getter  between {low,high}
 double random(double low,double high);
 
 double distanceVector(const vector<double>& v1, const vector<double>& v2);
