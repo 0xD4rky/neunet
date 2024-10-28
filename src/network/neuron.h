d98af8c0894a354ee8aec1bd34f8d993610c3159
0xD4rky <ishaan.kumar281@gmail.com>
2024-10-28 23:44:28 +0530

    redefining neuron

--- src/network/neuron.h
+++ src/network/neuron.h
@@ -1,24 +1,11 @@
-#ifndef NEURON_H
-#define NEURON_H
-
+#pragma once
 #include <vector>
-#include "functions.h"
 
-class Neuron{
-    public:
-        Neuron(int input_size);
-        double activate(const std::vector<double>& inputs);
-        
-        // accessors to update and get W&Bs
-
-        std::vector<double>& get_weights();
-        double& get_bias();
-        void update(const std::vector<double>& delta_weights, double delta_bias, double learning_rate);
-
-    private:
-        std::vector<double> weights;
-        double bias;
-};
-
-
-#endif
+class Neuron {
+public:
+    Neuron(size_t numInputs);
+    double activate(const std::vector<double>& inputs);
+    void updateWeights(const std::vector<double>& inputs, double delta, double learningRate);
+    std::vector<double> getWeights() const;
+    double getDelta() const;
+    void setDelta(double delta);
