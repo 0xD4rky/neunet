64c4b06d1144822742e0cf81b5089924b327c738
ishaan <ishaan.kumar281@gmail.com>
2024-11-28 21:45:48 +0530

    updating network

--- src/network/net.cpp
+++ src/network/net.cpp
@@ -1,3 +1,13 @@
 #include "network.h"
 #include "../util/utils.h"
 #include <iostream>
+
+
+NeuralNetwork::NeuralNetwork(const std::vector<size_t>& topology, 
+                           double learningRate) 
+    : learningRate_(learningRate) {
+    
+    for (size_t i = 0; i < topology.size() - 1; i++) {
+        layers_.emplace_back(topology[i + 1], topology[i]);
+    }
+}
